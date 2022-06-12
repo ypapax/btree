2a61629a4fd270ec26180fa85358ac865caf8d8f
Maxim Yefremov <maxYefr@gmail.com>
2022-06-12 17:40:40 +0300

    works for small

--- btree.go
+++ btree.go
@@ -2,7 +2,9 @@ package btree
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"math"
 	"math/rand"
 	"time"
 
@@ -34,6 +36,9 @@ func Random(nodes, maxVal int) *Btree {
 }
 
 func (bt *Btree) Add(val int) *Btree {
+	if bt.Value == val {
+		return bt
+	}
 	if val <= bt.Value {
 		if bt.Left == nil {
 			bt.Left = &Btree{Value: val}
@@ -62,3 +67,136 @@ func Parse(str string) (*Btree, error) {
 	}
 	return &tree, nil
 }
+
+type printBtreeObj struct {
+	Btree
+	isLeft bool
+	isRight bool
+	Row    int
+	Column int
+	Empty bool
+	Parent *printBtreeObj
+}
+
+func (bt *Btree) Print() {
+	m := bt.GetPrintMatrix()
+	PrintMatrix(m)
+}
+
+func PrintMatrix(result [][]string) {
+	var maxWidth int
+	for _, row := range result {
+		for _, el := range row {
+			if len(el) > maxWidth {
+				maxWidth = len(el)
+			}
+		}
+	}
+	for _, row := range result {
+		var line string
+		for _, el := range row {
+			elSpace := addSpaces(maxWidth, el)
+			line += elSpace
+		}
+		//log.Println("len(line): ", len(line), "len(row)", len(row), "len(result)", len(result))
+		log.Println(line)
+	}
+}
+const spaceChar = " "
+func addSpaces(maxWidth int, el string) string {
+	spaceLenLeft := maxWidth - len(el)
+	var space string
+	for i := 0; i < spaceLenLeft/2; i++ {
+		space += spaceChar
+	}
+	return space + el + space
+}
+
+func (bt *Btree) GetPrintMatrix() (result [][]string) {
+	if bt == nil {
+		return nil
+	}
+	var byLevel = make(map[int][]printBtreeObj)
+	var cur printBtreeObj
+	curLevel := 0
+	q := printBtreeObj{Btree: *bt}
+	byLevel[curLevel] = append(byLevel[curLevel], q)
+	var queue []printBtreeObj
+	queue = append(queue, q)
+	var maxLevel int
+	var maxElementsOnRow = 1
+	for {
+		if len(queue) == 0 {
+			break
+		}
+		cur = queue[0]
+		queue = queue[1:]
+		newLevel := cur.Row + 1
+		var gotChildren bool
+		if cur.Left != nil {
+			ql := printBtreeObj{Btree: *cur.Left, isLeft: true, Row: newLevel, Parent: &cur, Column: cur.Column - 1}
+			byLevel[newLevel] = append(byLevel[newLevel], ql)
+			maxLevel = newLevel
+			queue = append(queue, ql)
+			gotChildren = true
+		} else {
+			byLevel[newLevel] = append(byLevel[newLevel], printBtreeObj{isLeft: true, Empty: true, Parent: &cur, Column: cur.Column - 1})
+		}
+		if cur.Right != nil {
+			qr := printBtreeObj{Btree: *cur.Right, isRight: true, Row: newLevel, Parent: &cur, Column: cur.Column+1}
+			byLevel[newLevel] = append(byLevel[newLevel], qr)
+			maxLevel = newLevel
+			queue = append(queue, qr)
+			gotChildren = true
+		} else {
+			byLevel[newLevel] = append(byLevel[newLevel], printBtreeObj{isRight: true, Empty: true, Parent: &cur, Column: cur.Column+1})
+		}
+		if gotChildren {
+			maxElementsOnRow *= 2
+		}
+	}
+	maxElementsOnRowIncludingSpace := maxElementsOnRow * 2
+	log.Println("maxElementsOnRow", maxElementsOnRow)
+	maxElOnTheRow := 1
+	log.Println("maxLevel", maxLevel)
+	result = make([][]string, maxLevel+1)
+	headColumn := maxElementsOnRowIncludingSpace / 2
+	for level := 0; level <= maxLevel; level++ {
+		result[level] = make([]string, maxElementsOnRowIncludingSpace)
+
+		els := byLevel[level]
+		spaceFloat := float64(maxElementsOnRow- maxElOnTheRow)/2
+		space := /*centerColumn+*/int(math.Round(spaceFloat))
+		log.Println("space", space, "els", len(els), "maxElOnTheRow", maxElOnTheRow, "spaceFloat", spaceFloat)
+		var spaceLeft string
+		for i := 0; i < space; i++ {
+			spaceLeft += " "
+		}
+		var row = spaceLeft
+		for j, el := range els {
+			var valStr string
+			/*if el.Parent != nil {
+				byLevel[level][i].Column = el.Parent.Column + 1
+			}*/
+			if el.Btree.Value > 0 {
+				valStr = fmt.Sprintf("%+v", el.Btree.Value)
+			}
+			column := headColumn+el.Column
+			if len(result) <= level {
+				panic(fmt.Sprintf("not enough lines count %+v, len(result): %+v", level, len(result)))
+			}
+			if len(result[level]) <= column {
+				panic(fmt.Sprintf("not enough columns %+v, len(result[level]): %+v for level %+v, j: %+v, len(els): %+v, headColumn: %+v",
+					column, len(result[level]), level, j, len(els), headColumn))
+
+			}
+			result[level][column] = valStr
+			//row = row +  fmt.Sprintf("%+v %+v", spaceLeft, valStr)
+		}
+		row += spaceLeft
+		log.Println(row)
+		maxElOnTheRow *= 2
+	}
+	return result
+}
+
--- btree_test.go
+++ btree_test.go
@@ -6,9 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestRand(t *testing.T) {
+	tree := Random(10, 100)
+	tree.Print()
+}
+
+func TestPrint(t *testing.T) {
+	tree := New(7).Add(77).Add(5).Add(8)//.Add(3).Add(1).Add(10).Add(10)
+	tree.Print()
+	//log.Println(tree.String())
+}
+
 func TestAdd(t *testing.T) {
 	as := assert.New(t)
-	tree := New(7).Add(5).Add(8).Add(3).Add(1).Add(10)
+	tree := New(7).Add(5).Add(8).Add(3).Add(1).Add(10).Add(10)
 	expStr := `{
            "right/": {
               "right/": {
@@ -31,7 +42,7 @@ func TestAdd(t *testing.T) {
 	if !as.NoError(err) {
 		return
 	}
-
+	tree.Print()
 	t.Log(tree.String())
 	as.Equal(expTree, tree)
 }
